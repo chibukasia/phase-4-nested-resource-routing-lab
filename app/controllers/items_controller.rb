ec3153eff6dfcf5275ac1c37f2798e9a43eee0a0
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-29 21:25:46 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end 
+
+  def create 
+    item = Item.create(items_params)
+    render json: item, status: :created
+  end 
+
+
+  # Private methods 
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response 
+    render json: {error: "Item not found"}, status: :not_found
+  end 
+
+  def render_unprocessable_entity_response(err)
+    render json: {error: err.record.errors}, status: :unprocessable_entity
   end
 
 end
